83fc2aa17300adcd98656c3c9dc6e1038a5c468c
DESKTOP-A0PDOAP\Wei Kang <lweikang96@gmail.com>
2018-06-23 23:42:13 +0800

    Fixed FirebaseUI, added tabLayout and Toolbar

--- app/src/main/java/com/weikang/getindutch/MainPage.java
+++ app/src/main/java/com/weikang/getindutch/MainPage.java
@@ -4,7 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -16,34 +23,47 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Arrays;
 
 public class MainPage extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 1;
+    public static final int RC_SIGN_IN = 1;
+    public static final String GUEST = "guest";
+    public static final String TAG = "MainPage";
 
     private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListerner;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     private ProgressBar mProgressBar;
     private Spinner mSortDropdown;
     private FloatingActionButton mAddButton;
     private String mUsername;
+    private ListView mItemListView;
+    private Toolbar mToolbar;
+
+    private SectionsPageAdapter mSectionsPageAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Log.d(TAG, "On create: Starting.");
 
+
+        mUsername = GUEST;
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
         //initialising buttons
         mAddButton = (FloatingActionButton) findViewById(R.id.addBtn);
         mSortDropdown = (Spinner) findViewById(R.id.dropDown);
+        mItemListView = (ListView) findViewById(R.id.itemListView);
+        mToolbar = (Toolbar) findViewById(R.id.mainPageToolbar);
+        setSupportActionBar(mToolbar);
 
-        mAuthStateListerner = new FirebaseAuth.AuthStateListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
                     //user is signed in
-                    Toast.makeText(MainPage.this,"You've signed in successfully!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainPage.this,"Welcome to GetInDutch!", Toast.LENGTH_SHORT).show();
                 } else {
                     //user is signed out
                     startActivityForResult(
@@ -74,16 +94,41 @@ public class MainPage extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainpagemenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.sign_out_btn:
+                //sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
         if (mFirebaseAuth != null){
-            mFirebaseAuth.removeAuthStateListener(mAuthStateListerner);
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListerner);
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    private void setupViewPage(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new SummaryPage(),"Summary");
+        viewPager.setAdapter(adapter);
     }
 }
