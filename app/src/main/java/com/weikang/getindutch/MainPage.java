b918e8e2aa66de7e63ae4f3ed5bf7b2b44e47141
DESKTOP-A0PDOAP\Wei Kang <lweikang96@gmail.com>
2018-06-23 16:14:21 +0800

    2nd commit

--- app/src/main/java/com/weikang/getindutch/MainPage.java
+++ app/src/main/java/com/weikang/getindutch/MainPage.java
@@ -1,13 +1,89 @@
 package com.weikang.getindutch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainPage extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListerner;
+    private ProgressBar mProgressBar;
+    private Spinner mSortDropdown;
+    private FloatingActionButton mAddButton;
+    private String mUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        //initialising buttons
+        mAddButton = (FloatingActionButton) findViewById(R.id.addBtn);
+        mSortDropdown = (Spinner) findViewById(R.id.dropDown);
+
+        mAuthStateListerner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    //user is signed in
+                    Toast.makeText(MainPage.this,"You've signed in successfully!", Toast.LENGTH_SHORT).show();
+                } else {
+                    //user is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mFirebaseAuth != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListerner);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListerner);
     }
 }
