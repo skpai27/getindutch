ce21a02da23afa4f1ee7ecb78c3824252f5bde46
Si Kai <skpai27@yahoo.com.sg>
2018-07-09 00:10:35 +0800

    third commit
    under addexpenses, included getting list of groups to populate group spinner, algorithm to split expense among all users in group.

--- app/src/main/java/com/weikang/getindutch/AddExpenses.java
+++ app/src/main/java/com/weikang/getindutch/AddExpenses.java
@@ -1,8 +1,10 @@
 package com.weikang.getindutch;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +23,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddExpenses extends AppCompatActivity {
 
     private Spinner mPayeeSpinner;
@@ -27,7 +33,9 @@ public class AddExpenses extends AppCompatActivity {
     private EditText mExpense;
     private Button mButtonAdd;
     private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mUserGroupsDatabaseReference;
     private DatabaseReference mGroupDatabaseReference;
+    private DatabaseReference mMembersDatabaseReference;
     private ChildEventListener mChildEventListener;
 
     private String selectedGroup;
@@ -42,6 +50,9 @@ public class AddExpenses extends AppCompatActivity {
         FirebaseUser mUser = mAuth.getCurrentUser();
         final String mUserId = mUser.getUid();
 
+        //Firebase Database initialisation
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
         mExpense = findViewById(R.id.edittext_Expense);
         //initialise button
         mButtonAdd = findViewById(R.id.button_add);
@@ -49,7 +60,6 @@ public class AddExpenses extends AppCompatActivity {
 
         //Configuring Payee Spinner
         mPayeeSpinner = findViewById(R.id.spinner_payee);
-
         //create arrayadapter using String array and a default spinner layout
         ArrayAdapter<CharSequence> payeeSpinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.payee_array, android.R.layout.simple_spinner_item);
@@ -58,9 +68,24 @@ public class AddExpenses extends AppCompatActivity {
 
         //Configuring Group Spinner
         mGroupSpinner = findViewById(R.id.spinner_group);
-        //create arrayadapter using String array and a default spinner layout
-        ArrayAdapter<CharSequence> groupSpinnerAdapter = ArrayAdapter.createFromResource(this,
-                R.array.group_array, android.R.layout.simple_spinner_item);
+        //create List of groups that user belongs to
+        final List<String> groups = new ArrayList<String>();
+        //groups.add("alalala");
+        mUserGroupsDatabaseReference = mFirebaseDatabase.getReference().child("users").child(mUserId).child("groups");
+        mUserGroupsDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot groupSnapshot: dataSnapshot.getChildren()) {
+                    String groupName = groupSnapshot.getKey();
+                    groups.add(groupName);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+        System.out.println(groups.toArray());
+        //create arrayadapter using the list above and a default spinner layout
+        ArrayAdapter<String> groupSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, groups);
         groupSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mGroupSpinner.setAdapter(groupSpinnerAdapter);
 
@@ -68,6 +93,9 @@ public class AddExpenses extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView parent, View view, int position, long id){
                 selectedGroup = parent.getItemAtPosition(position).toString();
+                System.out.println(selectedGroup);
+                //((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+                //((TextView) parent.getChildAt(0)).setTextSize(5);
             }
             @Override
             public void onNothingSelected(AdapterView parent){
@@ -77,12 +105,13 @@ public class AddExpenses extends AppCompatActivity {
 
         mButtonAdd.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Float expense = Float.parseFloat(mExpense.getText().toString());
+                final Float expense = Float.parseFloat(mExpense.getText().toString());
 
                 //Firebase Database variables
-                mFirebaseDatabase = FirebaseDatabase.getInstance();
+
                 //get reference to the group that was selected
                 mGroupDatabaseReference = mFirebaseDatabase.getReference().child("groups").child(selectedGroup).child("members");
+                mMembersDatabaseReference = mGroupDatabaseReference.child("members");
 
                 //get size of group: (sk: added size attribute to group in database for this)
                 //use array of size1 to retrieve data from firebase cos of some variables problem
@@ -96,11 +125,38 @@ public class AddExpenses extends AppCompatActivity {
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {}
                 });
-                Float expenseToAdd = expense / sizeOfGroup[0];
+                final Float expenseToAdd = expense / sizeOfGroup[0];
+
+                //iterating through each member in the group using childeventlistener
+                mChildEventListener = new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        //if current member is the USER himself
+                        if(dataSnapshot.getKey() == mUserId){
+                            float newValue = Integer.parseInt(dataSnapshot.getValue().toString()) + expense - expenseToAdd;
+                            mMembersDatabaseReference.child(dataSnapshot.getKey()).setValue(newValue);
+                        }
+                        else{
+                            float newValue = Integer.parseInt(dataSnapshot.getValue().toString()) - expenseToAdd;
+                            mMembersDatabaseReference.child(dataSnapshot.getKey()).setValue(newValue);
+                        }
+                    }
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {}
+                };
+                mMembersDatabaseReference.addChildEventListener(mChildEventListener);
+                mMembersDatabaseReference.removeEventListener(mChildEventListener);//redundancy
+
                 //HARDCODED
-                mGroupDatabaseReference.child(mUserId).setValue(expense);
+                //mGroupDatabaseReference.child(mUserId).setValue(expense);
                 //below is test1 userid
-                mGroupDatabaseReference.child("XIihVerGHgRgqKfAeL2vs9gbkf02").setValue(expense * -1);
+                //mGroupDatabaseReference.child("XIihVerGHgRgqKfAeL2vs9gbkf02").setValue(expense * -1);
 
                 //TODO: retrieve size of group, divide expense, reduce users expense, add to other members expense
                 //get original balances\
